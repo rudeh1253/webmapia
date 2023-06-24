675ea2dc15ed1cdd2a380211c25d3ca5b9a584d0
pgd <rudeh1253@gmail.com>
2023-06-24 11:17:43 +0900

    chore: Initialize server project

---
+++ webmapia-server/src/main/java/com/nsl/webmapia/WebmapiaApplication.java
@@ -0,0 +1,13 @@
+package com.nsl.webmapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmapiaApplication.class, args);
+	}
+
+}
