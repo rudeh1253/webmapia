9fb999c9d284ba97350808b178c1122244d707eb
pgd <rudeh1253@gmail.com>
2023-06-28 23:58:59 +0900

    add: interface for skill

---
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/domain/skill/Skill.java
@@ -0,0 +1,18 @@
+package com.nsl.webmapia.game.domain.skill;
+
+import com.nsl.webmapia.game.domain.SkillInfo;
+
+/**
+ * Classes which implements this interface defines the behavior of skill.
+ */
+public interface Skill {
+
+    /**
+     * Use skill based on the character.
+     * @param targetUserId user id of target of the skill. targetUserId should be included
+     *                     into the set of the participants.
+     * @return information of expected result of the skill. The result may vary by the other skill used by
+     *         other user.
+     */
+    SkillInfo useSkill(Long targetUserId);
+}
