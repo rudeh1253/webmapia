d91aa67fa5da1efa07e307a3c93e9619fed47237
pgd <rudeh1253@gmail.com>
2023-06-30 23:02:13 +0900

    add: character object represents murderer

---
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/domain/character/Murderer.java
@@ -0,0 +1,36 @@
+package com.nsl.webmapia.game.domain.character;
+
+import com.nsl.webmapia.common.exception.CharacterNotSupportSkillTypeException;
+import com.nsl.webmapia.game.domain.skill.SkillEffect;
+import com.nsl.webmapia.game.domain.skill.SkillType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Murderer implements Character {
+    private static final CharacterCode CHARACTER_CODE = CharacterCode.MURDERER;
+    private static final Faction FACTION = Faction.HUMAN;
+    private boolean canKill = true;
+
+    @Override
+    public SkillEffect activateSkill(SkillType skillType) {
+        if (skillType != SkillType.EXTERMINATE) {
+            throw new CharacterNotSupportSkillTypeException("Murderer doesn't support given skill type: SkillType code"
+                    + skillType);
+        }
+        SkillEffect result = new SkillEffect();
+        result.setSkillType(skillType);
+        result.setSkillCondition((src, tar, type) -> canKill);
+        result.setOnSkillSucceed((src, tar, type) -> canKill = false);
+        return result;
+    }
+
+    @Override
+    public CharacterCode getCharacterCode() {
+        return CHARACTER_CODE;
+    }
+
+    @Override
+    public Faction getFaction() {
+        return FACTION;
+    }
+}
