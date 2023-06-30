846b5253765ba512974b58b442b6d2eecfcfbade
pgd <rudeh1253@gmail.com>
2023-07-01 00:02:01 +0900

    feat: implement Follower

--- webmapia-server/src/main/java/com/nsl/webmapia/game/domain/character/Follower.java
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/domain/character/Follower.java
@@ -1,6 +1,8 @@
 package com.nsl.webmapia.game.domain.character;
 
 import com.nsl.webmapia.common.exception.CharacterNotSupportSkillTypeException;
+import com.nsl.webmapia.game.domain.CharacterEffectAfterNightType;
+import com.nsl.webmapia.game.domain.notification.SkillNotificationBody;
 import com.nsl.webmapia.game.domain.skill.SkillEffect;
 import com.nsl.webmapia.game.domain.skill.SkillType;
 import org.springframework.stereotype.Component;
@@ -9,15 +11,57 @@ import org.springframework.stereotype.Component;
 public class Follower implements Character {
     private static final CharacterCode CHARACTER_CODE = CharacterCode.FOLLOWER;
     private static final Faction FACTION = Faction.WOLF;
+    private boolean isAvailableInsight = true;
 
     @Override
     public SkillEffect activateSkill(SkillType skillType) {
-        if (skillType != SkillType.INVESTIGATE_ALIVE_CHARACTER) {
-            throw new CharacterNotSupportSkillTypeException("Follower doesn't support given skill type: SkillType code "
-                    + skillType);
+        SkillEffect result = new SkillEffect();
+        result.setSkillType(skillType);
+        switch (skillType) {
+            case INVESTIGATE_ALIVE_CHARACTER:
+                insight(result);
+                break;
+            case ENTER_WOLF_CHAT:
+                enterChat(result);
+                break;
+            default:
+                throw new CharacterNotSupportSkillTypeException("Follower doesn't support given skill type: SkillType code "
+                        + skillType);
         }
-        // TODO: set skill condition
-        return null;
+        return result;
+    }
+
+    private void insight(SkillEffect result) {
+        result.setSkillCondition((src, tar, type) -> true);
+        result.setOnSkillSucceed((src, tar, type) -> src.addMessageAfterNight(SkillNotificationBody.builder()
+                .receiverUserId(src.getID())
+                .skillTargetUserId(tar.getID())
+                .skillTargetCharacterCode(tar.getCharacter().getCharacterCode())
+                .characterEffectAfterNightType(CharacterEffectAfterNightType.INVESTIGATE)
+                .build()));
+    }
+
+    private void enterChat(SkillEffect result) {
+        result.setSkillCondition((src, tar, type) -> tar.getCharacter().getCharacterCode() == CharacterCode.WOLF);
+        result.setOnSkillSucceed((src, tar, type) -> {
+            SkillNotificationBody srcBody = SkillNotificationBody.builder()
+                    .receiverUserId(src.getID())
+                    .skillTargetUserId(tar.getID())
+                    .skillTargetCharacterCode(tar.getCharacter().getCharacterCode())
+                    .characterEffectAfterNightType(CharacterEffectAfterNightType.ENTER_WOLF_CHAT)
+                    .build();
+
+            SkillNotificationBody tarBody = SkillNotificationBody.builder()
+                    .receiverUserId(tar.getID())
+                    .skillTargetUserId(null)
+                    .skillTargetCharacterCode(null)
+                    .characterEffectAfterNightType(CharacterEffectAfterNightType.NOTIFY)
+                    .message("Betrayer entered the wolf chat")
+                    .build();
+
+            src.addMessageAfterNight(srcBody);
+            tar.addMessageAfterNight(tarBody);
+        });
     }
 
     @Override
