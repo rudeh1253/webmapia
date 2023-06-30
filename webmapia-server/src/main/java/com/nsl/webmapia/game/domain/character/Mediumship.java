55a7df6bb1015ed8b795708da5721b891aa39543
pgd <rudeh1253@gmail.com>
2023-06-30 21:29:51 +0900

    add: character object represents mediumship

---
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/domain/character/Mediumship.java
@@ -0,0 +1,50 @@
+package com.nsl.webmapia.game.domain.character;
+
+import com.nsl.webmapia.common.exception.CharacterNotSupportSkillTypeException;
+import com.nsl.webmapia.game.domain.CharacterEffectAfterNightType;
+import com.nsl.webmapia.game.domain.notification.SkillNotificationBody;
+import com.nsl.webmapia.game.domain.skill.SkillEffect;
+import com.nsl.webmapia.game.domain.skill.SkillType;
+
+public class Mediumship implements Character {
+    private static final CharacterCode CHARACTER_CODE = CharacterCode.MEDIUMSHIP;
+    private static final Faction FACTION = Faction.HUMAN;
+
+    @Override
+    public SkillEffect activateSkill(SkillType skillType) {
+        if (skillType != SkillType.INVESTIGATE_DEAD_CHARACTER) {
+            throw new CharacterNotSupportSkillTypeException("Mediumship doesn't support given skill type:"
+                    + "SkillType code: " + skillType);
+        }
+        SkillEffect result = new SkillEffect();
+        result.setSkillType(skillType);
+        result.setSkillCondition((src, tar, type) -> true);
+        result.setOnSkillSucceed((src, tar, type) -> {
+            SkillNotificationBody notificationBody = SkillNotificationBody.builder()
+                    .skillTargetUserId(tar.getID())
+                    .characterEffectAfterNightType(CharacterEffectAfterNightType.INVESTIGATE)
+                    .receiverUserId(src.getID())
+                    .build();
+            CharacterCode targetCharacterCode = tar.getCharacter().getCharacterCode();
+            if (targetCharacterCode == CharacterCode.WOLF
+            || targetCharacterCode == CharacterCode.GUARD
+            || targetCharacterCode == CharacterCode.PREDICTOR) {
+                notificationBody.setSkillTargetCharacterCode(targetCharacterCode);
+            } else {
+                notificationBody.setSkillTargetCharacterCode(CharacterCode.GOOD_MAN);
+            }
+            src.addMessageAfterNight(notificationBody);
+        });
+        return result;
+    }
+
+    @Override
+    public CharacterCode getCharacterCode() {
+        return CHARACTER_CODE;
+    }
+
+    @Override
+    public Faction getFaction() {
+        return FACTION;
+    }
+}
