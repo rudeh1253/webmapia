4d12cb28fecd6ad2b10046f6b807e6c1cc9f50f9
pgd <rudeh1253@gmail.com>
2023-07-03 15:50:55 +0900

    feat: implment GameServer interface

--- webmapia-server/src/main/java/com/nsl/webmapia/game/service/GameService.java
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/service/GameService.java
@@ -1,7 +1,10 @@
 package com.nsl.webmapia.game.service;
 
+import com.nsl.webmapia.game.domain.Vote;
 import com.nsl.webmapia.game.domain.character.Character;
 import com.nsl.webmapia.game.domain.notification.PrivateNotificationBody;
+import com.nsl.webmapia.game.domain.skill.SkillEffect;
+import com.nsl.webmapia.game.domain.skill.SkillType;
 
 import java.util.List;
 
@@ -23,7 +26,37 @@ public interface GameService {
      */
     void stepForward();
 
-    void addUser(Long userId);
+    /**
+     * Accept vote.
+     * @param vote Vote object
+     */
+    void acceptVote(Vote vote);
 
+    /**
+     * Add user in repository. The id is generated randomly.
+     * The value of id is greater than 0.
+     * @return id generated.
+     */
+    Long addUser();
+
+    /**
+     * Remove user from the repository.
+     * @param userId of user to remove.
+     * @return user id of user removed if the user exists, otherwise -1.
+     */
     Long removeUser(Long userId);
+
+    /**
+     * Process activation of a skill.
+     * @param activatorId of user which activated skill.
+     * @param targetId of user which is the target of the skill.
+     * @param skillType of skill activated.
+     */
+    void activateSkill(Long activatorId, Long targetId, SkillType skillType);
+
+    /**
+     * After the night, process activated skills.
+     * @return a list of skill effects.
+     */
+    List<SkillEffect> processSkills();
 }
