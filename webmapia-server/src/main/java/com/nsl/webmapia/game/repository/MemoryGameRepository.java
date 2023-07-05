672dd72ed3ee61b7e8d826886a667916fe02c808
pgd <rudeh1253@gmail.com>
2023-07-05 23:37:35 +0900

    feat: implement GameRepsitory using memory

---
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/repository/MemoryGameRepository.java
@@ -0,0 +1,47 @@
+package com.nsl.webmapia.game.repository;
+
+import com.nsl.webmapia.game.domain.GameManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryGameRepository implements GameRepository {
+    private final Map<Long, GameManager> store;
+
+    public MemoryGameRepository() {
+        this.store = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public void save(GameManager game) {
+        store.put(game.getGameId(), game);
+    }
+
+    @Override
+    public Optional<GameManager> findById(Long gameId) {
+        return Optional.ofNullable(store.get(gameId));
+    }
+
+    @Override
+    public List<GameManager> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public boolean containsKey(Long gameId) {
+        return store.containsKey(gameId);
+    }
+
+    @Override
+    public int countAll() {
+        return store.size();
+    }
+
+    @Override
+    public Optional<GameManager> deleteGameById(Long gameId) {
+        return Optional.ofNullable(store.remove(gameId));
+    }
+}
