21cdf104a17bd05bea2b666746040733164d8918
pgd <rudeh1253@gmail.com>
2023-07-07 20:31:36 +0900

    feat: start to implement GameController

--- webmapia-server/src/main/java/com/nsl/webmapia/game/controller/GameController.java
+++ webmapia-server/src/main/java/com/nsl/webmapia/game/controller/GameController.java
@@ -1,9 +1,15 @@
 package com.nsl.webmapia.game.controller;
 
+import com.nsl.webmapia.game.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class GameController {
+    private GameService gameService;
 
-
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
 }
