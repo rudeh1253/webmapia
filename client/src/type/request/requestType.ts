07f78c0520d1a51a59ca6e4f96b4070d682936cc
pgd <rudeh1253@gmail.com>
2023-08-02 16:31:09 +0900

    feat: add types for request and response

--- client/src/type/request/requestType.ts
+++ client/src/type/request/requestType.ts
@@ -2,20 +2,50 @@ export type CharacterGenerationRequest = {
     notificationType: string;
     gameId: number;
     characterDistribution: {
-        "WOLF": number;
-        "BETRAYER": number;
-        "FOLLOWER": number;
-        "PREDICTOR": number;
-        "GUARD": number;
-        "MEDIUMSHIP": number;
-        "DETECTIVE": number;
-        "SUCCESSOR": number;
-        "SECRET_SOCIETY": number;
-        "NOBILITY": number;
-        "SOLDIER": number;
-        "TEMPLAR": number;
-        "CITIZEN": number;
-        "MURDERER": number;
-        "HUMAN_MOUSE": number;
-    }
+        WOLF: number;
+        BETRAYER: number;
+        FOLLOWER: number;
+        PREDICTOR: number;
+        GUARD: number;
+        MEDIUMSHIP: number;
+        DETECTIVE: number;
+        SUCCESSOR: number;
+        SECRET_SOCIETY: number;
+        NOBILITY: number;
+        SOLDIER: number;
+        TEMPLAR: number;
+        CITIZEN: number;
+        MURDERER: number;
+        HUMAN_MOUSE: number;
+    };
+};
+
+export type GameStartNotificationRequest = {
+    notificationType: string;
+    gameSetting: string;
+    gameId: number;
+};
+
+export type PhaseEndRequest = {
+    notificationType: string;
+    gameId: number;
+    userId: number;
+};
+
+export type PostPhaseRequest = {
+    notificationType: string;
+    gameId: number;
+};
+
+export type VoteRequest = {
+    notificationType: string;
+    gameId: number;
+    voterId: number;
+    subjectId: number;
+};
+
+export type RoomCreationRequest = {
+    gameName: string;
+    hostId: number;
+    hostName: string;
 };
--- client/src/type/response/responseType.ts
+++ client/src/type/response/responseType.ts
@@ -3,9 +3,55 @@ export type CharacterGenerationResponse = {
     receiverId: number;
     characterCode: string;
     gameId: number;
-}
+};
 
 export type CurrentPhaseResponse = {
     gameId: number;
     gamePhase: string;
-}
+};
+
+export type PhaseEndNotificationResponse = {
+    notificationType: string;
+    gameId: number;
+    isEnd: boolean;
+};
+
+export type PhaseResultResponse = {
+    notificationType: string;
+    gameId: number;
+    isGameEnd: boolean;
+    winner: string;
+};
+
+export type VoteResultResponse = {
+    notificationType: string;
+    gameId: number;
+    idOfUserToBeExecuted: number;
+};
+
+export type RoomInfoResponse = {
+    notificationType: string;
+    roomId: number;
+    hostId: number;
+    roomName: string;
+    users: UserResponse[];
+};
+
+export type UserResponse = {
+    notificationType: string;
+    gameId: number;
+    userId: number;
+    username: string;
+    characterCode: string;
+    isDead: boolean;
+};
+
+export type SkillResultResponse = {
+    notificationType: string;
+    gameId: number;
+    skillTargetId: number;
+    skillActivatorId: number;
+    receiverId: number;
+    message: string;
+    characterEffectAfterNightType: string;
+};
