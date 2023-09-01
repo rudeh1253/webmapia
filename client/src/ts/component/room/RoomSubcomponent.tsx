08c8dc713e70b7f66059991a8d5f639f968c642a
pgd <rudeh1253@gmail.com>
2023-09-01 16:57:28 +0900

    refactor: extract new component

--- client/src/ts/component/room/RoomSubcomponent.tsx
+++ client/src/ts/component/room/RoomSubcomponent.tsx
@@ -5,185 +5,195 @@ import {setGameConfigurationModal} from "../../redux/slice/gameConfigurationModa
 import {Chat, UserInfo} from "../../type/gameDomainType";
 
 export function GameConfigurationModal() {
+    return (
+        <div className="configuration-modal-container">
+            <TimeConfiguration />
+        </div>
+    );
+}
+
+function TimeConfiguration() {
     const gameConfiguration = useAppSelector((state) => state.gameConfiugraion);
     const dispatch = useAppDispatch();
     return (
-        <div className="configuration-modal-container">
-            <div className="radio-container">
-                <div className="radio-component">
-                    <h6 className="radio-title">
-                        {strResource.room.gameConfigurationModal.discussionTime}
-                    </h6>
-                    <div className="radio-group">
-                        <div className="radio-item">
-                            <label htmlFor="discussion-time-60">60</label>
-                            <input
-                                type="radio"
-                                name="discussion-time"
-                                id="discussion-time-60"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            discussionTimeSeconds: 60
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                        <div className="radio-item">
-                            <label htmlFor="discussion-time-90">90</label>
-                            <input
-                                type="radio"
-                                name="discussion-time"
-                                id="discussion-time-90"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            discussionTimeSeconds: 90
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                        <div className="radio-item">
-                            <label htmlFor="discussion-time-120">120</label>
-                            <input
-                                type="radio"
-                                name="discussion-time"
-                                id="discussion-time-120"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            discussionTimeSeconds: 120
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
+        <div className="radio-container">
+            <div className="radio-component">
+                <h6 className="radio-title">
+                    {strResource.room.gameConfigurationModal.discussionTime}
+                </h6>
+                <div className="radio-group">
+                    <div className="radio-item">
+                        <label htmlFor="discussion-time-60">60</label>
+                        <input
+                            type="radio"
+                            name="discussion-time"
+                            id="discussion-time-60"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        discussionTimeSeconds: 60
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                    <div className="radio-item">
+                        <label htmlFor="discussion-time-90">90</label>
+                        <input
+                            type="radio"
+                            name="discussion-time"
+                            id="discussion-time-90"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        discussionTimeSeconds: 90
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                    <div className="radio-item">
+                        <label htmlFor="discussion-time-120">120</label>
+                        <input
+                            type="radio"
+                            name="discussion-time"
+                            id="discussion-time-120"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        discussionTimeSeconds: 120
+                                    })
+                                )
+                            }
+                        />
                     </div>
                 </div>
-                <div className="radio-component">
-                    <h6 className="radio-title">
-                        {strResource.room.gameConfigurationModal.voteTime}
-                    </h6>
-                    <div className="radio-group">
-                        <div className="radio-item">
-                            <label htmlFor="vote-time-60">60</label>
-                            <input
-                                type="radio"
-                                name="vote-time"
-                                id="vote-time-60"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            voteTimeSeconds: 60
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                        <div className="radio-item">
-                            <label htmlFor="vote-time-90">90</label>
-                            <input
-                                type="radio"
-                                name="vote-time"
-                                id="vote-time-90"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            voteTimeSeconds: 90
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                        <div className="radio-item">
-                            <label htmlFor="vote-time-120">120</label>
-                            <input
-                                type="radio"
-                                name="vote-time"
-                                id="vote-time-120"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            voteTimeSeconds: 120
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="radio-component">
-                    <h6 className="radio-title">
-                        {strResource.room.gameConfigurationModal.nightTime}
-                    </h6>
-                    <div className="radio-group">
-                        <div className="radio-item">
-                            <label htmlFor="night-time-60">60</label>
-                            <input
-                                type="radio"
-                                name="night-time"
-                                id="night-time-60"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            nightTimeSeconds: 60
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                        <div className="radio-item">
-                            <label htmlFor="night-time-90">90</label>
-                            <input
-                                type="radio"
-                                name="night-time"
-                                id="night-time-90"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            nightTimeSeconds: 90
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                        <div className="radio-item">
-                            <label htmlFor="night-time-120">120</label>
-                            <input
-                                type="radio"
-                                name="night-time"
-                                id="night-time-120"
-                                onChange={() =>
-                                    dispatch(
-                                        setGameConfiguration({
-                                            ...gameConfiguration,
-                                            nightTimeSeconds: 120
-                                        })
-                                    )
-                                }
-                            />
-                        </div>
-                    </div>
-                </div>
-                <button
-                    type="button"
-                    onClick={() => dispatch(setGameConfigurationModal(false))}
-                >
-                    {strResource.room.gameConfigurationModal.close}
-                </button>
             </div>
+            <div className="radio-component">
+                <h6 className="radio-title">
+                    {strResource.room.gameConfigurationModal.voteTime}
+                </h6>
+                <div className="radio-group">
+                    <div className="radio-item">
+                        <label htmlFor="vote-time-60">60</label>
+                        <input
+                            type="radio"
+                            name="vote-time"
+                            id="vote-time-60"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        voteTimeSeconds: 60
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                    <div className="radio-item">
+                        <label htmlFor="vote-time-90">90</label>
+                        <input
+                            type="radio"
+                            name="vote-time"
+                            id="vote-time-90"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        voteTimeSeconds: 90
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                    <div className="radio-item">
+                        <label htmlFor="vote-time-120">120</label>
+                        <input
+                            type="radio"
+                            name="vote-time"
+                            id="vote-time-120"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        voteTimeSeconds: 120
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="radio-component">
+                <h6 className="radio-title">
+                    {strResource.room.gameConfigurationModal.nightTime}
+                </h6>
+                <div className="radio-group">
+                    <div className="radio-item">
+                        <label htmlFor="night-time-60">60</label>
+                        <input
+                            type="radio"
+                            name="night-time"
+                            id="night-time-60"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        nightTimeSeconds: 60
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                    <div className="radio-item">
+                        <label htmlFor="night-time-90">90</label>
+                        <input
+                            type="radio"
+                            name="night-time"
+                            id="night-time-90"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        nightTimeSeconds: 90
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                    <div className="radio-item">
+                        <label htmlFor="night-time-120">120</label>
+                        <input
+                            type="radio"
+                            name="night-time"
+                            id="night-time-120"
+                            onChange={() =>
+                                dispatch(
+                                    setGameConfiguration({
+                                        ...gameConfiguration,
+                                        nightTimeSeconds: 120
+                                    })
+                                )
+                            }
+                        />
+                    </div>
+                </div>
+            </div>
+            <button
+                type="button"
+                onClick={() => dispatch(setGameConfigurationModal(false))}
+            >
+                {strResource.room.gameConfigurationModal.close}
+            </button>
         </div>
     );
+}
+
+function CharacterConfiguration() {
+    
 }
 
 export function ChatItem({senderId, message, timestamp, isMe}: Chat) {
