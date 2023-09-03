f0e03de423a2f416012d76dccc852e22dfc14f95
pgd <rudeh1253@gmail.com>
2023-09-03 12:51:08 +0900

    refactor: remove unused code

--- client/src/ts/component/room/game/DistributionPhase.tsx
+++ client/src/ts/component/room/game/DistributionPhase.tsx
@@ -1,30 +1,6 @@
-import {useEffect, useState} from "react";
 import strResource from "../../../../resource/string.json";
-import {useAppSelector} from "../../../redux/hook";
-import {GamePhase} from "../../../type/gameDomainType";
-import SocketClient from "../../../sockjs/SocketClient";
-import GameManager from "../../../game/GameManager";
-import {SOCKET_SEND_PHASE_END} from "../../../util/const";
-import {PhaseEndRequest} from "../../../type/requestType";
-
-var sockClient: SocketClient;
-var gameManager: GameManager;
 
 export default function DistributionPhase() {
-    const [distributed, setDistributed] = useState(false);
-
-    const gamePhase = useAppSelector((state) => state.currentGamePhase);
-    const thisUser = useAppSelector((state) => state.thisUserInfo);
-    const roomInfo = useAppSelector((state) => state.currentRoomInfo);
-
-    useEffect(() => {
-        if (!sockClient) {
-            SocketClient.getInstance().then((result) => (sockClient = result));
-        }
-        if (!gameManager) {
-            gameManager = GameManager.getInstance();
-        }
-    }, []);
     return (
         <div className="game-container distribution-phase">
             <p className="distribution-message">
