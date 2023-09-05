954ef0b2d46a47eab05d9ef035f40b90a1485d61
pgd <rudeh1253@gmail.com>
2023-09-05 09:56:08 +0900

    refactor: extract code related to onPhaseEnd

--- client/src/ts/util/getSubscription.tsx
+++ client/src/ts/util/getSubscription.tsx
@@ -14,7 +14,6 @@ import {
 } from "../type/responseType";
 import {CurrentRoomInfoInitialState} from "../redux/slice/currentRoomInfoSlice";
 import {
-    GAME_PHASE_ORDER,
     SOCKET_SEND_PHASE_END,
     SOCKET_SUBSCRIBE_CHATROOM_PRIVATE,
     SOCKET_SUBSCRIBE_CHATROOM_PUBLIC,
@@ -29,10 +28,9 @@ import GameManager from "../game/GameManager";
 import SocketClient from "../sockjs/SocketClient";
 import {PhaseEndRequest} from "../type/requestType";
 import {NotificationType} from "../type/notificationType";
-import {setCurrentGamePhase} from "../redux/slice/currentGamePhaseSlice";
-import {setTimeCount} from "../redux/slice/timeCountSlice";
+import {onPhaseEnd} from "./onPhaseEnd";
 
-var gameManager = GameManager.getInstance();
+export var gameManager = GameManager.getInstance();
 
 export function getSubscription(
     currentRoomInfo: CurrentRoomInfoInitialState,
@@ -191,62 +189,4 @@ async function onCharacterAllocationResponse(
         userId: thisUser.userId
     };
     sockClient.sendMessage(SOCKET_SEND_PHASE_END, {}, body);
-}
-
-function onPhaseEnd(
-    currentGamePhase: GamePhase,
-    dispatch: any,
-    gameConfiguartion: GameSetting
-) {
-    const nextPhase = getNextPhase(currentGamePhase);
-    dispatch(setCurrentGamePhase(nextPhase));
-    taskOnNextPhase(nextPhase, dispatch);
-}
-
-function getNextPhase(currentGamePhase: GamePhase) {
-    const idx = GAME_PHASE_ORDER.indexOf(currentGamePhase);
-    const nextIdx = (idx % (GAME_PHASE_ORDER.length - 1)) + 1;
-    return GAME_PHASE_ORDER[nextIdx];
-}
-
-function taskOnNextPhase(
-    nextPhase: GamePhase,
-    dispatch: any
-) {
-    const gameConfiguration = gameManager.gameSetting;
-    let howMany;
-    switch (nextPhase) {
-        case GamePhase.DAYTIME:
-            howMany = gameConfiguration.discussionTimeSeconds;
-            dispatch(setTimeCount(howMany));
-            startTimeCount(howMany, dispatch);
-            break;
-        case GamePhase.VOTE:
-            howMany = gameConfiguration.voteTimeSeconds;
-            dispatch(setTimeCount(howMany));
-            startTimeCount(howMany, dispatch);
-            break;
-        case GamePhase.NIGHT:
-            howMany = gameConfiguration.nightTimeSeconds;
-            dispatch(setTimeCount(howMany));
-            startTimeCount(howMany, dispatch);
-            break;
-        default:
-            const DEFAULT_COUNT = 90;
-            dispatch(setTimeCount(DEFAULT_COUNT));
-            startTimeCount(DEFAULT_COUNT, dispatch);
-    }
-}
-
-function startTimeCount(count: number, dispatch: any) {
-    if (count <= 0) {
-        return;
-    }
-
-    const SECOND_IN_MILLIS = 1000;
-    setTimeout(() => {
-        const c = count - 1;
-        dispatch(setTimeCount(c));
-        startTimeCount(c, dispatch);
-    }, SECOND_IN_MILLIS);
 }
