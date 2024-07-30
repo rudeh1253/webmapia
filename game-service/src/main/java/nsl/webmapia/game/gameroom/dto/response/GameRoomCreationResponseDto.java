1ac86ef69fb7ed0bdec37e470b585ede43fe3d2f
pgd <rudeh1253@gmail.com>
2024-07-30 19:04:19 +0900

    feat: define GameRoomService interface
    Related to: #8

---
+++ game-service/src/main/java/nsl/webmapia/game/gameroom/dto/response/GameRoomCreationResponseDto.java
@@ -0,0 +1,10 @@
+package nsl.webmapia.game.gameroom.dto.response;
+
+import java.time.LocalDateTime;
+
+public class GameRoomCreationResponseDto {
+    private int roomId;
+    private String roomName;
+    private String hostMemberId;
+    private LocalDateTime creationTime;
+}
