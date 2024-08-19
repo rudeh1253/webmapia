9eafa144a759e9b7fae8c2b29f4a9f934192c2a9
pgd <rudeh1253@gmail.com>
2024-08-19 14:53:57 +0900

    feat: add methods to CharacterAssignmentRepository
    Methods to query alive/dead characters given gameRoomId
    
    Related to: #16

--- game-service/src/main/java/nsl/webmapia/game/character/repository/CharacterAssignmentRepository.java
+++ game-service/src/main/java/nsl/webmapia/game/character/repository/CharacterAssignmentRepository.java
@@ -21,6 +21,8 @@ public interface CharacterAssignmentRepository {
 
     List<CharacterAssignment> findDeadCharacterAssignmentsByGameRoomId(int gameRoomId);
 
+    List<CharacterAssignment> findAliveCharacterAssignmentsByGameRoomId(int gameRoomId);
+
     Optional<CharacterAssignment> findByGameInstanceIdAndMemberId(int gameInstanceId, String memberId);
 
     /**
