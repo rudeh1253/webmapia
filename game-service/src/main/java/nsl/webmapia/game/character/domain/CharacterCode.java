d0e82ccea8e77acdaaa0b50dfaa1475fe836ac08
pgd <rudeh1253@gmail.com>
2024-08-19 14:55:14 +0900

    feat: implement PredictorCharacterDefinitionService
    Related to: #16

--- game-service/src/main/java/nsl/webmapia/game/character/domain/CharacterCode.java
+++ game-service/src/main/java/nsl/webmapia/game/character/domain/CharacterCode.java
@@ -4,7 +4,7 @@ public enum CharacterCode {
     WOLF("늑대"),
     BETRAYER("배신자"),
     FOLLOWER("추종자"),
-//    PREDICTOR,
+    PREDICTOR("예언자"),
     GUARD("경비병"),
     MEDIUMSHIP("영매"),
     DETECTIVE("탐정"),
