1ee4d9ee61c9169a3a2b8d437d8c4721520fbf88
pgd <rudeh1253@gmail.com>
2024-08-20 16:50:32 +0900

    feat(GameService): write specification for distributing characters
    Related to: #29

--- game-service/src/main/java/nsl/webmapia/game/gameoperation/service/GameService.java
+++ game-service/src/main/java/nsl/webmapia/game/gameoperation/service/GameService.java
@@ -1,7 +1,9 @@
 package nsl.webmapia.game.gameoperation.service;
 
 import nsl.webmapia.game.gameoperation.dto.VoteDto;
+import nsl.webmapia.game.gameoperation.dto.request.CharacterDistributionRequestDto;
 import nsl.webmapia.game.gameoperation.dto.request.VoteRequestDto;
+import nsl.webmapia.game.gameoperation.dto.response.CharacterDistributionResponseDto;
 import nsl.webmapia.game.gameoperation.dto.response.PhaseResultResponseDto;
 
 import java.util.List;
@@ -23,6 +25,19 @@ public interface GameService {
     Integer startGame(int roomId);
 
     /**
+     * For each participant of the game instance, assign a character to the participant.
+     * If sum of CharacterDistributionsRequestDto.numByCharacters.values() is less than the number of
+     * participants, then the rest will be assigned to CITIZEN.
+     *
+     * @param dto request DTO containing information about for each character how many members will be assigned to that
+     * @return a DTO represents distribution of characters
+     * @throws IllegalArgumentException if CharacterDistributionsRequestDto.numByCharacters.values() exceeds the number
+     *                                  of members which participate in the game instance
+     */
+    CharacterDistributionResponseDto distributeCharacters(CharacterDistributionRequestDto dto)
+            throws IllegalArgumentException;
+
+    /**
      * Process a vote from a single member. The size of a single vote is determined by the character of
      * the member owns.
      *
@@ -35,7 +50,7 @@ public interface GameService {
      * Request to end the phase. When all members request to end the phase, the phase ends.
      *
      * @param gameInstanceId of the GameInstance
-     * @param requesterId of Member who requested to end the phase
+     * @param requesterId    of Member who requested to end the phase
      * @return PhaseResultResponseDto that contains information of the result of the phase
      */
     PhaseResultResponseDto endPhase(int gameInstanceId, String requesterId);
