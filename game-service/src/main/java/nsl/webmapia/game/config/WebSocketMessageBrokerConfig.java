2c68d3db14fb15ecacf0f9fbcb441d9b40b831e8
pgd <rudeh1253@gmail.com>
2024-09-12 16:30:25 +0900

    chore: configure WebSocket
    Related to: #39

--- game-service/src/main/java/nsl/webmapia/game/config/WebSocketMessageBrokerConfig.java
+++ game-service/src/main/java/nsl/webmapia/game/config/WebSocketMessageBrokerConfig.java
@@ -1,5 +1,6 @@
 package nsl.webmapia.game.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,15 +15,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketMessageBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${client.origin}")
+    private String clientOrigin;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // TODO: In the future, more sophisticated configuration will be needed.
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/game-service").setAllowedOrigins(this.clientOrigin).withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
+        registry.setApplicationDestinationPrefixes("/app/game-service");
         registry.enableSimpleBroker("/topic", "/queue");
         registry.setUserDestinationPrefix("/user");
     }
