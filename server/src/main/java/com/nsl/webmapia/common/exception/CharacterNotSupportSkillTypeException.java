c4a7f05c4d5cf360aace2d668c0448d607df4bed
pgd <rudeh1253@gmail.com>
2024-09-29 19:47:11 +0900

    chore: remove legacy server-project

--- server/src/main/java/com/nsl/webmapia/common/exception/CharacterNotSupportSkillTypeException.java
+++
@@ -1,8 +0,0 @@
-package com.nsl.webmapia.common.exception;
-
-public class CharacterNotSupportSkillTypeException extends RuntimeException {
-
-    public CharacterNotSupportSkillTypeException(String message) {
-        super(message);
-    }
-}
--- server/src/main/java/com/nsl/webmapia/user/repository/UserIdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nsl.webmapia.user.repository;
-
-public interface UserIdRepository {
-    void addId(Long id);
-    boolean exist(Long id);
-    void remove(Long id);
-}
